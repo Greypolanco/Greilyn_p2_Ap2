729f125a7b0486f66ebb5f662ed013b908e1e3bc
Greypolanco <greilyntejada03@gmail.com>
2023-11-09 22:05:07 -0400

    agregando entidades en el Dto

--- app/src/main/java/com/example/greilyn_p2_ap2/data/remote/dto/GastosDto.kt
+++ app/src/main/java/com/example/greilyn_p2_ap2/data/remote/dto/GastosDto.kt
@@ -6,8 +6,10 @@ data class GastosDto(
     @Json(name = "idGasto")
     val gastoId : Int? = null,
     var fecha : String,
-    var suplidor : String,
+    var suplidor : String? = null,
+    val idSuplidor : Int,
     var concepto : String,
+    var descuento : Int = 0,
     var ncf : String,
     var itbis : String,
     val monto : Int
